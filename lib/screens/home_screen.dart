85cbd3178bf457275d1c5e3b1157c5888fec84da
Brendon Huang <brendonhuang12@gmail.com>
2026-03-02 14:59:20 -0500

    feat: built HomeScreen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../data/recipes_data.dart';
+import 'details_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Recipe Book"),
+      ),
+      body: ListView.builder(
+        itemCount: sampleRecipes.length,
+        itemBuilder: (context, index) {
+          final recipe = sampleRecipes[index];
+
+          return Card(
+            margin: const EdgeInsets.all(10),
+            child: ListTile(
+              leading: Image.asset(
+                recipe.imagePath,
+                width: 60,
+                fit: BoxFit.cover,
+              ),
+              title: Text(recipe.name),
+              trailing: const Icon(Icons.arrow_forward),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => DetailsScreen(recipe: recipe),
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
